b7a1fd239ef25baa6754ab8ac733bd56378cb82e
matiimonti <matilde.monti@edu.em-lyon.com>
2025-06-16 22:14:59 -0400

    Changes in cleaning code

---
+++ SRC/utils.py
@@ -0,0 +1,43 @@
+# Imports
+import pandas as pd
+
+
+##### Cleaning data functions #####
+
+def clean_data(df, threshold, do_imputation = False):
+    # Remove columns with many missing values
+    missing_ratio = df.isnull().mean()
+    columns_to_drop = missing_ratio[missing_ratio > threshold].index.tolist()  # threshold to be decided based on the dataset and the column
+    final_df = df.drop(columns = columns_to_drop)
+
+    # If imputation is enabled, apply value-based imputation on category columns
+    if do_imputation == True:
+        final_df = value_impute(final_df, 'primary_category', 'secondary_category')
+        final_df = value_impute(final_df, 'secondary_category', 'tertiary_category')
+
+    # Drop null values and duplicates
+    final_df = final_df.dropna(axis=0)
+    final_df = final_df.drop_duplicates()
+
+    return final_df
+
+
+# Function to impute the values based on similar columns: if there's a missing value, the function imputes the most common value for each category, or returns the same value
+def value_impute(df, col, target_col):
+    df = df.copy()
+
+    mode_map = (
+        df
+        .dropna(subset=[target_col])
+        .groupby(col)[target_col]
+        .agg(lambda x: x.mode().iloc[0])
+    )
+    def impute_column(row):
+        if pd.isnull(row[target_col]):
+            return mode_map.get(row[col], row[col])
+        else:
+            return row[target_col]
+    df[target_col] = df.apply(impute_column, axis=1)
+    return df
+
+
